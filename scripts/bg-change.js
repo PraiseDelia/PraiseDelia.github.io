622d8f6740cc23b16d61ec56fa0bc746d16d88f9
PraiseDelia <65369099+PraiseDelia@users.noreply.github.com>
2020-10-02 14:54:13 -0700

    update

--- scripts/bg-change.js
+++ scripts/bg-change.js
@@ -2,10 +2,12 @@
 var change = document.body;
 
 function bgChange(){
-    var C1 = Math.floor(Math.random() * 300)
-    var C2 = Math.floor(Math.random() * 230)
-    var C3 = Math.floor(Math.random() * 300)
+    var range = Math.ceil(Math.random() * 8)
+    var C1 = (range * 25)
+    var C2 =(range * 20)
+    var C3 = (range * 30)
     var bg = "rgb(" + C1 + ", " +  C2 + ", " + C3 + ")" 
     change.style.backgroundColor = bg;
+    
 };
 setInterval(bgChange, 2000)
