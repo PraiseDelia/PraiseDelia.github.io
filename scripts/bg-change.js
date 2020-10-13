c4a5222e3112e16fe0c0bedec9bb0e07f6dd796f
PraiseDelia <elemdelia@gmail.com>
2020-10-12 20:06:59 -0700

    simplified bg-changes

--- scripts/bg-change.js
+++ scripts/bg-change.js
@@ -1,11 +1,7 @@
-
 var change = document.body;
 
 function bgChange(){
-    var C1 = Math.floor(Math.random() * 300)
-    var C2 = Math.floor(Math.random() * 230)
-    var C3 = Math.floor(Math.random() * 300)
-    var bg = "rgb(" + C1 + ", " +  C2 + ", " + C3 + ")" 
-    change.style.backgroundColor = bg;
+    var Colours = ["green", "red", "yellow", "pink", "crimson", "blue", "orange", "purple"]
+    change.style.backgroundColor = Colours[ Math.floor(Math.random() * 8)];
 };
 setInterval(bgChange, 2000)
