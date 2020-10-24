a848cb1ffdfda98d2dbe801b1e5c350a384fa37b
PraiseDelia <elemdelia@gmail.com>
2020-10-23 20:15:45 -0700

    trying to find and replace

--- scripts/Word.js
+++ scripts/Word.js
@@ -1,1 +1,11 @@
-document.getElementById("text").style.max-width = "85%"
+
+var toReplace = document.querySelector(".find");
+
+toReplace.addEventListener("click", function(){
+    var find = document.getElementById("text").value;
+    var collection = [find]
+    for(access of collection){
+        var replace = access.replace("hello", "hi")
+        document.getElementById("text").value = replace;
+    }
+})
