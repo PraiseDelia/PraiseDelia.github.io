3ba9fa4ed25311771a5f67e65237aa25d9e6083b
PraiseDelia <elemdelia@gmail.com>
2020-10-24 19:18:03 -0700

    still trying to find and replace

--- scripts/Word.js
+++ scripts/Word.js
@@ -2,10 +2,12 @@
 var toReplace = document.querySelector(".find");
 
 toReplace.addEventListener("click", function(){
-    var find = document.getElementById("text").value;
-    var collection = [find]
-    for(access of collection){
-        var replace = access.replace("hello", "hi")
-        document.getElementById("text").value = replace;
-    }
+   var find = document.getElementById("text").value;
+   var splitUp = find.split(" ");
+   for(var i = 0; i < splitUp.length; i++){
+      var collection = splitUp[i]
+      var replacements = [collection.replace("hello", "hi")]
+      var reCollect = replacements.join("  ")
+      document.getElementById("text").value = reCollect;
+   }
 })
