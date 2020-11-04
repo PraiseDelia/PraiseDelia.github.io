89852c3568d8e22c692e1b38e3d4aed54bc0ea35
PraiseDelia <elemdelia@gmail.com>
2020-11-03 20:27:42 -0800

    my CGPR calculator made well

--- scripts/CGPR.js
+++ scripts/CGPR.js
@@ -1,24 +1,7 @@
+var collect = []
+var unitSum = []
 class GPCalc{
-    constructor(){
-        this.environ = document.createElement("div");
-        this.myTable = document.createElement("table");
-        this.phase1 = document.createElement("tr");
-        this.header1 = document.createElement("th");
-        this.header1.innerHTML = "Course";
-        this.header2 = document.createElement("th");
-        this.header2.innerHTML = "Credit Load";
-        this.header3 = document.createElement("th");
-        this.header3.innerHTML = "Your Grade";
-        this.header4 = document.createElement("th");
-        this.header4.innerHTML = "Submit"
-        this.phase1.appendChild(this.header1);
-        this.phase1.appendChild(this.header2);
-        this.phase1.appendChild(this.header3);
-        this.phase1.appendChild(this.header4);
-        this.environ.appendChild(this.phase1);
-        document.body.appendChild(this.environ);
-    }
-    addMore(){
+    constructor(course, gradeNumber, CreditL ){
         this.phase2 = document.createElement("tr");
         this.row1 = document.createElement("td")
         this.innerRow1 = document.createElement("input");
@@ -40,38 +23,61 @@ class GPCalc{
         this.row3.appendChild(this.innerRow3);
         this.phase2.appendChild(this.submit);
         this.submit.appendChild(this.innerSubmit)
-        this.environ.appendChild(this.phase2)
-
+        document.body.appendChild(this.phase2)
+        
         var ideal = this;
         this.submit.addEventListener("click", ()=>{
-            var CreditL = ideal.innerRow2.value;
-            var Grade = ideal.innerRow3.value;
-            var gradeNumber;
-            switch (Grade) {
+            ideal.CreditL = ideal.innerRow2.value;
+            ideal.Grade = ideal.innerRow3.value;
+            ideal.gradeNumber;
+            switch (ideal.Grade) {
                 case "A":
-                    gradeNumber = 5;
+                    ideal.gradeNumber = 5;
                     break;
                 case "B":
-                    gradeNumber = 4;
+                    ideal.gradeNumber = 4;
                     break;
                 case "C":
-                    gradeNumber = 3;
+                    ideal.gradeNumber = 3;
                     break;
                 case "D":
-                    gradeNumber = 2;
+                    ideal.gradeNumber = 2;
                     break;
                 case "E":
-                    gradeNumber = 1;
+                    ideal.gradeNumber = 1;
                     break;
                 default:
-                    gradeNumber = 0;
+                    ideal.gradeNumber = 0;
             }
-        ideal.total = gradeNumber * CreditL
-        check.addMore();
+            ideal.total = ideal.gradeNumber * ideal.CreditL
+            collect.push(ideal.total);
+            ideal.string = parseInt(ideal.CreditL)
+            unitSum.push(ideal.string);
         });
-
     }
-   
 }
-var check = new GPCalc()
-check.addMore()
+
+var addNew = document.createElement("button")
+addNew.innerHTML = "Add another Course"
+document.body.appendChild(addNew)
+addNew.addEventListener("click", ()=> {
+    var subject = new GPCalc(course, gradeNumber, CreditL);
+    var course = subject.innerRow1.value;
+    var gradeNumber = subject.gradeNumber;
+    var CreditL = subject.CreditL
+    
+})
+    
+var calc = document.createElement("button")
+calc.innerHTML = "Calculate GP"
+document.body.appendChild(calc)
+calc.addEventListener("click", ()=> {
+    var sum = 0;
+    var totalUnit = 0;
+    for(var i = 0; i < collect.length; i++){
+        sum += collect[i]
+        totalUnit += unitSum[i]
+    }
+    var GP = sum / totalUnit;
+    document.querySelector(".GPHolder").innerHTML = "Your CGPA = "+ GP
+})
