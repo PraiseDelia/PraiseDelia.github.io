81d52846d2df7915b6053414af98b965808ae81d
PraiseDelia <elemdelia@gmail.com>
2020-11-05 22:09:16 -0800

    finally modified it

--- scripts/CGPR.js
+++ scripts/CGPR.js
@@ -1,11 +1,8 @@
 var collect = []
 
 class GPCalc{
-    constructor(course, gradeNumber, CreditL){
-        this.course = course;
-        this.gradeNumber = gradeNumber;
-        this.CreditL = CreditL;
-        this.product = gradeNumber * CreditL
+    constructor(){
+// this contains all the DOM elements
         this.phase2 = document.createElement("tr");
         this.row1 = document.createElement("td")
         this.innerRow1 = document.createElement("input");
@@ -34,31 +31,52 @@ class GPCalc{
     }
 }
 
+// this new button will create an object and push the entire object into the "collect array"
 var addNew = document.createElement("button")
 addNew.innerHTML = "Add another Course"
 document.body.appendChild(addNew)
 addNew.addEventListener("click", ()=> {
-    var subject = new GPCalc(course, gradeNumber, CreditL);
-    var course = subject.innerRow1.value;
-    var CreditL = subject.innerRow2.value;
-    var gradeNumber = subject.innerRow3.value;
-    
+    var subject = new GPCalc();
     collect.push(subject)
     
 })
-    
+ 
+//Here we access the needed properties of the objects in the array usin a for..of loop
 var calc = document.createElement("button")
 calc.innerHTML = "Calculate GP"
 document.body.appendChild(calc)
 calc.addEventListener("click", ()=> {
-    let sum = 0;
-    let totalUnits = 0;
+
+    let sum = 0; //holds product of creditLoad & Grade for all objects
+    let totalUnits = 0;//holds sum of creditLoads for all objects
     for(find of collect){
-        sum += parseInt(find.product);
-        totalUnits += parseInt(find.CreditL);
-        console.log(find.CreditL)
+        var Grade = find.innerRow3.value
+        var gradeNumber;
+// the switch will convert the letters to particular figures
+        switch (Grade) {
+            case "A":
+                gradeNumber = 5;
+                break;
+            case "B":
+                gradeNumber = 4;
+                break;
+            case "C":
+                gradeNumber = 3;
+                break;
+            case "D":
+                gradeNumber = 2;
+                break;
+            case "E":
+                gradeNumber = 1;
+            default:
+                gradeNumber = 0;
+                break;
+        }
+        var product = gradeNumber * find.innerRow2.value
+        sum += product
+        totalUnits += parseInt(find.innerRow2.value);
     }
-    console.log(totalUnits)
+    console.log(sum, totalUnits)
     var CGPA = sum/totalUnits;
     document.querySelector(".GPHolder").innerHTML = "CGPA = " + CGPA
    
